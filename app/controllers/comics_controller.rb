faa76fc92805c2aeb480985ffef7979722277232
Thomas <thomaswinkel612@gmail.com>
2015-08-04 16:02:42 -0300

    Modify commics controller

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -4,6 +4,7 @@
 # * Description       : Application For Entice users to watch ads by paying them, read books/comics and get coins.
 # * Developed By      : Bacancy Technology
 # * Developed In      : 2015-2016
+# * Modified By       : Thomas
 # +------------------------------------------------------------+
 class ComicsController < ApplicationController
   before_action :authenticate_user!, :except => [:show]
